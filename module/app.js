3bbc9d1d1fb9275a9f96cceabc08ca93b1b7308d
TristanHill <43789772+TristanHill@users.noreply.github.com>
2018-10-07 17:11:19 +0200

    Module+Unterordner

---
+++ module/app.js
@@ -0,0 +1,9 @@
+let data = require("./hallo.js");
+
+console.log(data.name("Hallo"));
+
+//require("./unterordner/app.js");
+
+require("./unterordner");
+
+
---
+++ module/hallo.js
@@ -0,0 +1,7 @@
+let data = {
+    name: function (prefix) {
+        return prefix + " Welt";
+    }
+};
+
+module.exports = data;
